742960d51048d161091bfb1123e3e25e91963c6b
PillowSky <ihyzi@163.com>
2014-09-04 10:44:38 +0800

    add sybbs

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * IDI
 * ZOJ
 * Deepin
+* SYBBS
---
+++ sybbs/sybbs/spiders/bbs.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from scrapy.http import Request
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from pyquery import PyQuery as pyq
+from pymongo import MongoClient
+
+class MainSpider(CrawlSpider):
+	name = "bbs"
+	allowed_domains = ['0575bbs.com']
+	start_urls = [
+		'http://www.0575bbs.com/',
+		'http://bbs.0575bbs.com/',
+		'http://food.0575bbs.com/',
+		'http://travel.0575bbs.com/',
+		'http://marry.0575bbs.com/',
+		'http://baby.0575bbs.com/',
+		'http://prepaid.0575bbs.com/',
+	]
+
+	rules = (
+		Rule(LinkExtractor(allow=('thread-htm-fid')), callback='parseField'),
+		Rule(LinkExtractor(allow=('read-htm-tid')), callback='parseThread'),
+	)
+
+	def __init__(self):
+		super(MainSpider, self).__init__()
+		self.db = MongoClient().sybbs
+		self.collection = self.db.thread
+
+	def parseField(self, response):
+
+		threads = []
+		for thread in response.css('tr.tr3'):
+			'''
+			Exceptions:
+			* stick top thread don't have field
+			* global stick top even don't have timestamp
+			* nested tags, use pyquery
+			* based whether on '-' in time to test if it is a recent thread
+			'''
+			try:
+				if '-' in thread.css('td.author > p > a::text').extract()[0]:
+					continue
+			except Exception, e:
+				continue
+
+			author = thread.css('td.author > p > a')
+			info ={
+				"url": self.start_urls[0] + author.css('::attr(href)').extract()[0].replace('-page-e', '').replace('#a', ''),
+				"timestamp": author.css('::attr(title)').extract()[0],
+			}
+
+			try:
+				info['title'] = pyq(thread.css('a.subject_t').extract()[0]).text()
+				info['field'] = thread.css('a.view::text').extract()[0].strip('[]')
+			except Exception, e:
+				pass
+
+			self.collection.update({"url": info['url']}, info, True)
+			threads.append(Request(info['url'], callback=self.parseThread))
+
+		return threads
+
+	def parseThread(self, response):
+		reply = []
+		for floor in response.css('div.tpc_content').extract():
+			reply.append(pyq(floor).text())
+
+		self.collection.update({"url": response.url}, {'$set': {"reply": reply}})
