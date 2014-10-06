ec46d28d8921e5ba723f2d62693f9f5b3c73ff8c
Yuzo <hyzgog@gmail.com>
2014-10-06 16:25:39 +0800

    implement up to 2007 year

--- sina/sina/spiders/finance.py
+++ sina/sina/spiders/finance.py
@@ -3,51 +3,87 @@ from scrapy.http import Request
 from scrapy.http import HtmlResponse
 from scrapy.spider import Spider
 from pyquery import PyQuery as pyq
+from pymongo import MongoClient
+from datetime import date, timedelta
 
 class FinanceSpider(Spider):
 	name = 'finance'
 
+	'''
+	intervals:
+		[1999.05.26, 1999.07.25) -> need right justify
+		[1999.07.25, 2001.02.01) -> needn't 
+		[2001.02.01, 2007.01.20) -> need right justify
+
+	'''
 	def __init__(self, category=None, *args, **kwargs):
 		super(FinanceSpider, self).__init__(*args, **kwargs)
 		self.allowed_domains = ['sina.com.cn']
+		self.db = MongoClient().sina
+		self.collection = self.db.finance
 
 	def start_requests(self):
-		# for days don't have content, sina will return 404 and will ignore by scrapy
-		for year in range(2007, 2007+1):
-			for month in range(1, 12+1):
-				for day in range(1, 31+1):
-					url = "http://news.sina.com.cn/old1000/news1000_%s%s%s.shtml" % (year, str(month).rjust(2, '0'), str(day).rjust(2, '0'))
-					yield Request(url, callback=self.parse2000List)
 
-	'''
-	def parse(self, response):
-		for url in response.css('a::attr(href)').extract():
-			if 'archives' in url:
-				if 'category' in url:
-					yield Request(url)
-				else:
-					if 'author' not in url:
-						yield Request(url, callback=self.parseTwo)
-	'''
+		today = date(1999, 5, 26)
+		step = timedelta(1)
+
+		while True:
+			url = "http://news.sina.com.cn/old1000/news1000_%s%s%s.shtml" % (today.year, str(today.month).rjust(2, '0'), str(today.day).rjust(2, '0'))
+			yield Request(url, callback=self.parse2000List)
+			today += step
+			if today.year == 1999 and today.month == 7 and today.day == 25:
+				break
+
+		while True:
+			url = "http://news.sina.com.cn/old1000/news1000_%s%s%s.shtml" % (today.year, today.month, today.day)
+			yield Request(url, callback=self.parse2000List)
+			today += step
+			if today.year == 2001 and today.month == 2 and today.day == 1:
+				break
+
+		while True:
+			url = "http://news.sina.com.cn/old1000/news1000_%s%s%s.shtml" % (today.year, str(today.month).rjust(2, '0'), str(today.day).rjust(2, '0'))
+			yield Request(url, callback=self.parse2000List)
+			today += step
+			if today.year == 2007 and today.month == 1 and today.day == 20:
+				break
 
 	def parse2000List(self, response):
 		response = response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8')
 		for row in response.css('li'):
 			if row.css('::text').extract()[0].encode('utf-8') == "[财经] ":
 				url = row.css('a::attr(href)').extract()[0]
-				yield Request(url, callback=self.parse2000Post)
+				yield Request(url, callback=self.parse2000Post, priority=10)
 
 	def parse2000Post(self, response):
-		response = response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8')
-		rawTitle = response.css('title::text').extract()[0]
-		d = pyq(response.css('div#artibody').extract()[0])
-		print d.text()
+		def analysys(response):
+			try:
+				d = pyq(response.css('div#artibody').extract()[0])
+				data = {
+					"url": response.url,
+					"title": response.css('h1#artibodyTitle::text').extract()[0],
+					"body": d.text(),
+					"date": response.css('span#pub_date::text').extract()[0],
+				}
+				return data
 
-		'''
-		data = {
-			url: response.url
-			title: rawTitle[:rawTitle.find('_')]
-			body: 
-		}
-		'''
+			except Exception, e:
+				print e
 
+			try:
+				d = pyq(response.css('td[valign="top"]').extract()[2])
+				data = {
+					"url": response.url,
+					"title": response.css('font[size="5"]::text').extract()[0],
+					"body": d.text(),
+					"date": response.css('font[face="Arial"]').extract()[0],
+				}
+				return data
+
+			except Exception, e:
+				print e
+
+			raise NotImplementedError('No matched analysys method')
+
+		data = analysys(response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8'))
+		self.collection.update({"url": data['url']}, {"$set": data}, True)
