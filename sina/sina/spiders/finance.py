da4acf3300a8242f14dc9b9514b97adc4981ca80
Yuzo <hyzgog@gmail.com>
2014-10-07 02:07:45 +0800

    resort code

--- sina/sina/spiders/finance.py
+++ sina/sina/spiders/finance.py
@@ -15,7 +15,7 @@ class FinanceSpider(Spider):
 		[1999.07.25, 2001.02.01) -> needn't 
 		[2001.02.01, 2007.01.20) -> need right justify
 		[2007.01.20, 2007.12.12) -> js, sort by time
-		[2007.12.12, 2014.10.05 -> js, by category
+		[2007.12.12, 2014.10.05) -> js, by category
 	'''
 	def __init__(self, category=None, *args, **kwargs):
 		super(FinanceSpider, self).__init__(*args, **kwargs)
@@ -25,27 +25,26 @@ class FinanceSpider(Spider):
 		self.collection = self.db.finance
 
 	def start_requests(self):
-
 		today = date(1999, 5, 26)
 		step = timedelta(1)
 
 		while True:
 			url = "http://news.sina.com.cn/old1000/news1000_%s%s%s.shtml" % (today.year, str(today.month).rjust(2, '0'), str(today.day).rjust(2, '0'))
-			yield Request(url, callback=self.parse2000List)
+			yield Request(url, callback=self.parseList)
 			today += step
 			if today.year == 1999 and today.month == 7 and today.day == 25:
 				break
 
 		while True:
 			url = "http://news.sina.com.cn/old1000/news1000_%s%s%s.shtml" % (today.year, today.month, today.day)
-			yield Request(url, callback=self.parse2000List)
+			yield Request(url, callback=self.parseList)
 			today += step
 			if today.year == 2001 and today.month == 2 and today.day == 1:
 				break
 
 		while True:
 			url = "http://news.sina.com.cn/old1000/news1000_%s%s%s.shtml" % (today.year, str(today.month).rjust(2, '0'), str(today.day).rjust(2, '0'))
-			yield Request(url, callback=self.parse2000List)
+			yield Request(url, callback=self.parseList)
 			today += step
 			if today.year == 2007 and today.month == 1 and today.day == 20:
 				break
@@ -59,17 +58,46 @@ class FinanceSpider(Spider):
 
 		while True:
 			url = "http://rss.sina.com.cn/rollnews/finance/%s%s%s.js" % (today.year, str(today.month).rjust(2, '0'), str(today.day).rjust(2, '0'))
-			yield Request(url, callback=self.parse2010Json)
+			yield Request(url, callback=self.parseJsObject)
 			today += step
 			if today.year == 2008 and today.month == 12 and today.day == 12:
 				break
 
-	def parse2000List(self, response):
+	def parseList(self, response):
 		response = response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8')
 		for row in response.css('li'):
 			if row.css('::text').extract()[0].encode('utf-8') == "[财经] ":
 				url = row.css('a::attr(href)').extract()[0]
 				yield Request(url, callback=self.parsePost, priority=10)
+
+	def parseJson(self, response):
+		url = bytearray(response.url)
+		i = url[-4] + 1
+		url[-4] = i
+		yield Request(str(url), callback=self.parseJson)
+
+		response = response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8')
+		body = response.body
+		start = body.index('[[')
+		end = body.index(']]') + 2
+		data = json.loads(body[start:end].replace('\\', ''), strict=False)
+		for item in data:
+			yield Request(item[3], callback=self.parsePost, priority=10)
+
+	def parseJsObject(self, response):
+		def dig(body):
+			lIndex = body.index('link') + 6
+			try:
+				while True:
+					lIndex = body.index('link', lIndex) + 6
+					rIndex = body.index('"', lIndex)
+					yield body[lIndex:rIndex]
+			except ValueError, e:
+				return
+
+		response = response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8')
+		for url in dig(response.body):
+			yield Request(url, callback=self.parsePost, priority=10)
 
 	def parsePost(self, response):
 		def analysys(response):
@@ -116,32 +144,3 @@ class FinanceSpider(Spider):
 
 		data = analysys(response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8'))
 		self.collection.update({"url": data['url']}, {"$set": data}, True)
-
-	def parseJson(self, response):
-		url = bytearray(response.url)
-		i = url[-4] + 1
-		url[-4] = i
-		yield Request(str(url), callback=self.parseJson)
-
-		response = response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8')
-		body = response.body
-		start = body.index('[[')
-		end = body.index(']]') + 2
-		data = json.loads(body[start:end].replace('\\', ''), strict=False)
-		for item in data:
-			yield Request(item[3], callback=self.parsePost, priority=10)
-
-	def parse2010Json(self, response):
-		def dig(body):
-			lIndex = body.index('link') + 6
-			try:
-				while True:
-					lIndex = body.index('link', lIndex) + 6
-					rIndex = body.index('"', lIndex)
-					yield body[lIndex:rIndex]
-			except ValueError, e:
-				return
-
-		response = response.replace(body=response.body.decode('gb2312', 'ignore').encode('utf-8'), encoding='utf-8')
-		for url in dig(response.body):
-			yield Request(url, callback=self.parsePost, priority=10)
